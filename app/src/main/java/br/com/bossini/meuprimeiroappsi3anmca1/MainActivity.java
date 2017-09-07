9c211457c4b3f918a487d4330cb60e637fff0606
rodrigo <rod.bossini@gmail.com>
2017-09-06 22:36:21 -0300

    Aula 1 de SI3ANMCA1. O pessoal não consegue ficar quieto.

---
+++ app/src/main/java/br/com/bossini/meuprimeiroappsi3anmca1/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.bossini.meuprimeiroappsi3anmca1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
